e17b8fc3b0f10faadc74436533a1c2fce9d10ce5
Mehebul02 <mehebul2123@gmail.com>
2024-08-10 19:31:40 +0600

    aside section added

--- src/components/header/TopHeader.tsx
+++ src/components/header/TopHeader.tsx
@@ -1,9 +1,17 @@
+import Container from '@/shared/Container';
 import React from 'react';
+import { CiDeliveryTruck } from 'react-icons/ci';
 
 const TopHeader = () => {
     return (
-        <div>
-            <h1>Top Header</h1>
+        <div className='bg-[#010f1c] '>
+            <Container className='flex items-center justify-between'>
+                <p className='text-white w-full md:mx-auto text-sm  flex items-center md:justify-normal justify-center '> <CiDeliveryTruck className='text-[#ffb342] text-2xl mr-1'/> FREE Express Shipping On Orders $570+</p>
+            <div>
+                <h1 className='text-white'>English</h1>
+                <h1 className='text-white'>English</h1>
+            </div>
+            </Container>
         </div>
     );
 };
