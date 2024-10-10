806def345344bd5595e8c2c63409519dbe684497
Mehebul02 <mehebul2123@gmail.com>
2024-10-10 23:16:39 +0600

    product added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Banner from '@/components/Banner';
+import { getData } from '@/helpers';
 
 
-const Home = () => {
+
+const  Home =async () => {
+
+  const endPoint = 'https://dummyjson.com/products'
+
+  const product= await getData(endPoint)
+  console.log(product);
+  
   return (
     <div>
      <Banner/>
