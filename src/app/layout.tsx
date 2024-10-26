8ed32c7f2bc7972d51748ba3b251e2a09c69ed4c
Mehebul02 <mehebul2123@gmail.com>
2024-10-26 20:56:05 +0600

    layout the redux added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/shared/Footer";
+import Layout from "@/components/Layout";
 // import Header from "@/components/header/Header"
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+       <Layout>
+       <Header />
         {children}
         <Footer/>
+       </Layout>
       </body>
     </html>
   );
