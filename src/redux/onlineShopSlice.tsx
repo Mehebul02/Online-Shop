7ad93a1828514f833707985cd06d795bafb5a89c
Mehebul02 <mehebul2123@gmail.com>
2024-10-26 22:04:47 +0600

    add redux

--- src/redux/onlineShopSlice.tsx
+++ src/redux/onlineShopSlice.tsx
@@ -1,22 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { Product } from "../../type";
 
-const initialState = {
+
+
+interface InitialState {
+    cart: Product[];
+    favorite: Product[];
+    userInfo: any;
+}
+
+const initialState: InitialState = {
     cart: [],
-    favorite:[],
-    userInfo:null
+    favorite: [],
+    userInfo: null
 }
 
 export const onlineShopSlice = createSlice({
     name: 'onlineShopSlice',
     initialState,
-    reducers:{
-        adToCart:(state,action)=>{
-            state.cart = action.payload
+    reducers: {
+        adToCart: (state, action) => {
+            const existingProduct = state?.cart.find((item) => item?.id === action.playload?.id)
+            // state.cart.push(action.payload)
 
         }
     }
 })
 
-export const {adToCart} = onlineShopSlice.actions
+export const { adToCart } = onlineShopSlice.actions
 
 export default onlineShopSlice.reducer
