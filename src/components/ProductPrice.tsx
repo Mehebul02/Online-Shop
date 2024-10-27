d9a3eac420542552495ebf8823d57016b99eca61
Mehebul02 <mehebul2123@gmail.com>
2024-10-27 22:55:31 +0600

    added the globle price

--- src/components/ProductPrice.tsx
+++ src/components/ProductPrice.tsx
@@ -1,14 +1,28 @@
-import { Product } from "../../type";
+'use client'
+
+import { useSelector } from "react-redux";
+import { Product, StateType } from "../../type";
 import PriceFormate from "./PriceFormate";
+import { useEffect, useState } from "react";
 
 const ProductPrice = ({product}:{product:Product}) => {
+
+    const { cart } = useSelector((state: StateType) => state?.onlineShop);
+    const [existingProduct, SetExistingProduct] = useState<Product | null>(null);
+    useEffect(()=>{
+        const availableProduct = cart?.find((item)=>item?.id === product?.id)
+        if(availableProduct){
+            SetExistingProduct(availableProduct)
+        }
+    },[cart , product])
+
     const regularPrice=product?.price;
     const discountPrice= product?.price+product?.discountPercentage/100;
     return (
         <div className="flex items-center gap-2">
             
            <PriceFormate className="text-gray-500 line-through font-normal" amount={regularPrice}/>
-           <PriceFormate className="font-semibold text-logoColor" amount={discountPrice}/>
+           <PriceFormate className="font-semibold text-logoColor" amount={existingProduct? discountPrice * existingProduct?. quantity!:discountPrice}/>
         </div>
     );
 };
