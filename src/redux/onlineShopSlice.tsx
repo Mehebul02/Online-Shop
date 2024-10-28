2e89c57ef82b4ccf72fe8909af450457a1127b0d
Mehebul02 <mehebul2123@gmail.com>
2024-10-28 22:37:19 +0600

    reset favorite cart impliment

--- src/redux/onlineShopSlice.tsx
+++ src/redux/onlineShopSlice.tsx
@@ -45,8 +45,12 @@ export const onlineShopSlice = createSlice({
         removeFromCart: (state, action) => {
             state.cart.filter((item) => item?.id! === action.payload)
         },
+        resetCart:(state)=>{
+            state.cart=[];
+
+        },
         // add to favorite 
-        adToFavorite: (state, action) => {
+        addToFavorite: (state, action) => {
             const existingProduct = state?.favorite?.find((item) => item?.id !== action.payload.id);
             if (existingProduct) {
                 state.favorite = state.favorite.filter((item) => item?.id !== action.payload.id)
@@ -55,10 +59,13 @@ export const onlineShopSlice = createSlice({
             else {
                 state.favorite.push(action.payload)
             }
+        },
+        resetFavorite:(state)=>{
+            state.favorite=[];
         }
     }
 })
 
-export const { adToCart, increaseQuantity, decreaseQuantity } = onlineShopSlice.actions
+export const { adToCart, increaseQuantity, decreaseQuantity,removeFromCart,resetCart, addToFavorite,resetFavorite} = onlineShopSlice.actions
 
 export default onlineShopSlice.reducer
